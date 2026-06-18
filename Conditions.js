26e8551462ef65d7e9b103e5e0f98457002459b2
RIDHIMA0120 <ridhima.25b01011081@abes.ac.in>
2026-06-18 21:48:09 +0530

    js

---
+++ Conditions.js
@@ -0,0 +1,39 @@
+// conditionals in JS : if , else if, else
+   /* let temperature=25;
+if (temperature > 30){
+    console.log ("It's hot!");
+
+} else if (temperature > 20){
+    console.log("it's warm");
+}
+else {
+    console.log("The weather is pleasant")
+}*/
+
+// switch case in JS:
+/*let day = "Tuesday";
+switch (day) {
+  case "Monday":
+    console.log("Start of the week!");
+    break;
+  case "Tuesday":
+    console.log("Keep going, it's only Tuesday!");
+    break;
+  case "Friday":
+    console.log("Finally, Friday!");
+    break;
+  default:
+    console.log("Just another day...");
+}*/
+
+
+// loops in JS:
+let student = {
+    Name : "rythm";
+    roll No. :12344;
+    email_id:"rythm@gmail.com"
+}
+c.1 (student[Name])
+
+//arrays in JS:
+
